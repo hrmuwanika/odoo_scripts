364b5005db111581f1b35680c0b9b4c89a373659
Henry Robert Muwanika <hrmuwanika@gmail.com>
2021-07-21 15:40:42 +0200

    Update backup_odoo.sh

--- backup_odoo.sh
+++ backup_odoo.sh
@@ -2,12 +2,12 @@
 
 # Setting Up Automatic Backup for Odoo Instance
 # mkdir -p /opt/script && cd /opt/script
-# wget https://raw.githubusercontent.com/hrmuwanika/odoo/master/backup_odoo.sh 
+# wget https://raw.githubusercontent.com/hrmuwanika/odoo_scripts/main/backup_odoo.sh 
 # sudo chmod +x backup_odoo.sh
 
-# To backup every 4 hours
+# To backup every 1 hours
 # sudo crontab -e
-# 0 */4 * * * /opt/script/backup_odoo.sh  
+# 0 */1 * * * /opt/script/backup_odoo.sh  
 
 # vars
 BACKUP_DIR=~/odoo_backups
